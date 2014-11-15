49912dd25b527f0e378636d22ba77241c339044c
Nakul <Nakul@Nakul-PC>
2014-11-15 17:05:55 -0500

    initial devs

--- gen/com/example/healthcompanion/R.java
+++ gen/com/example/healthcompanion/R.java
@@ -25,20 +25,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int editText1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
+        public static final int editText3=0x7f080002;
+        public static final int editText4=0x7f080003;
+        public static final int editText5=0x7f080004;
+        public static final int textView1=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
+        public static final int activity_form_data=0x7f030000;
+        public static final int activity_home=0x7f030001;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int form_data=0x7f070000;
+        public static final int home=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int temp_val=0x7f050004;
+        public static final int title_activity_form_data=0x7f050003;
     }
     public static final class style {
         /** 
